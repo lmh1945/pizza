0b68d43d4a1a291654f884c0d447c9f28e778a2e
lmh1945 <lmh1945@naver.com>
2016-12-19 21:13:38 +0900

    Update Menu.java

--- pizza/src/pizza/Menu.java
+++ pizza/src/pizza/Menu.java
@@ -11,17 +11,17 @@ public class Menu extends JFrame{
 	private JLabel label;
 	
 	public Menu(){
-		setBounds(1000, 400, 360, 350);		//프레임 크기지정
-		setTitle("Menu~");					//프레임 제목지정
+		setBounds(1000, 400, 360, 350);		
+		setTitle("Menu~");					
 		
-		JPanel menuPanel = new JPanel();	//패널 생성
-		Icon icon = new ImageIcon("menu.png");		//미리 저장해둔 메뉴판 사진을 아이콘에 저장
-		label = new JLabel();				//라벨생성
-		label.setIcon(icon);				//라벨에 아이콘을 넣어줌
-		add(menuPanel);						//프레임에 패널 등록
-		add(label);							//프레임에 라벨 등록
+		JPanel menuPanel = new JPanel();	
+		Icon icon = new ImageIcon("menu.png");		
+		label = new JLabel();				
+		label.setIcon(icon);				
+		add(menuPanel);						
+		add(label);							
 	
-		setVisible(true);					//프레임 화면을 띄어줌
+		setVisible(true);					
 	}
 	
 }
